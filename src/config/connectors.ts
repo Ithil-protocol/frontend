f644e4613794aff50c2fbddd8725cffbbcda4945
devneser <zh_alex@yahoo.com>
2022-07-11 15:28:26 +0800

    feat: add wallet connectors

---
+++ src/config/connectors.ts
@@ -0,0 +1,19 @@
+import { Goerli, Mainnet } from '@usedapp/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+
+import { infuraUrl } from '@/global/utils';
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [Mainnet.chainId, Goerli.chainId],
+});
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: {
+    [Mainnet.chainId]: infuraUrl(Mainnet.chainId),
+    [Goerli.chainId]: infuraUrl(Goerli.chainId),
+  },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  supportedChainIds: [Mainnet.chainId, Goerli.chainId],
+});
