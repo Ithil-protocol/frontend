1720b5042a9c444738ce9c88a1da7d51b2258b5d
devneser <zh_alex@yahoo.com>
2022-07-14 05:03:37 +0800

    refactor: remove page transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import tw from 'twin.macro';
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { useTransition, animated } from 'react-spring';
+import { Route, Routes } from 'react-router-dom';
 
 import Navbar from '@/components/navigation/Navbar';
 import APP_ROUTES from '@/config/routes';
 
 const App = () => {
-  const location = useLocation();
-  const transitions = useTransition(location, (locat) => locat.pathname, {
-    initial: { opacity: 1 },
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-    config: { duration: 300 },
-  });
-
   return (
     <div css={[tw`flex flex-col bg-primary min-h-screen desktop:flex-row`]}>
-      {transitions.map(({ item, props, key }) => (
-        <animated.div
-          tw="flex-grow flex flex-col"
-          style={{
-            ...props,
-            position: 'absolute',
-            height: '100%',
-            width: '100%',
-          }}
-          key={key}
-        >
-          <Navbar />
-          <Routes location={item}>
-            {APP_ROUTES.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.component}
-              />
-            ))}
-          </Routes>
-        </animated.div>
-      ))}
+      <div tw="flex-grow flex flex-col">
+        <Navbar />
+        <Routes>
+          {APP_ROUTES.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.component}
+            />
+          ))}
+        </Routes>
+      </div>
     </div>
   );
 };
