1a7fba3a79c4de66b59dbf9a4dd47430f4090623
OxMarco <>
2022-08-15 10:33:41 +0200

    remove urls

--- src/pages/faucets/Faucets.tsx
+++ src/pages/faucets/Faucets.tsx
@@ -31,9 +31,7 @@ export default function FaucetsPage() {
         </div>
         <div tw="flex flex-col justify-center items-center gap-3 self-center max-width[616px] text-center">
           <Txt.Body2Regular tw="text-secondary-200 mb-9">
-            Get our Goerli test tokens by using the below faucets. You will need
-            Goerli ETH to use our faucets (and all Ithil in testnet): you can
-            get some on{' '}
+            Get our test ERC20 tokens below
             <a
               href="https://goerlifaucet.com/"
               target="_blank"
