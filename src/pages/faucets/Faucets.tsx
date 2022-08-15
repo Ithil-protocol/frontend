0deb2d03170deddcf2d3ce96cf8befbba9cd2e10
OxMarco <>
2022-08-15 11:51:45 +0200

    fix text

--- src/pages/faucets/Faucets.tsx
+++ src/pages/faucets/Faucets.tsx
@@ -30,17 +30,6 @@ export default function FaucetsPage() {
           <Txt.Heading1 tw="mb-12"> Faucets </Txt.Heading1>
         </div>
         <div tw="flex flex-col justify-center items-center gap-3 self-center max-width[616px] text-center">
-          <Txt.Body2Regular tw="text-secondary-200 mb-9">
-            Get our test ERC20 tokens below
-            <a
-              href="https://goerlifaucet.com/"
-              target="_blank"
-              rel="noreferrer"
-              tw="hover:underline text-body2-bold"
-            >
-              https://goerlifaucet.com/
-            </a>
-          </Txt.Body2Regular>
           <DataTable
             head={[
               {
