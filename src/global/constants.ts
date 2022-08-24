2ef2a20afa74cd527c0a96324c5831a65b32615e
devneser <zh_alex@yahoo.com>
2022-08-25 03:11:08 +0800

    refactor: update yearn api url

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -68,8 +68,11 @@ export const DOC_URL = 'https://docs.ithil.fi';
 export const GITHUB_URL = 'https://github.com/Ithil-protocol';
 export const DISCORD_URL = 'https://discord.gg/tEaGBcGdQC';
 
-export const YEARN_API_URL =
-  'https://cors-anywhere.herokuapp.com/https://api.yearn.finance/v1/';
+export const YEARN_API_URL = `${
+  process.env.NODE_ENV === 'development'
+    ? 'https://cors-anywhere.herokuapp.com/'
+    : ''
+}https://api.yearn.finance/v1/`;
 
 export const TRADE_STRATEGIES = [
   {
