e0e152555d7ab9d798098d0e30748abf24b0c978
OxMarco <>
2022-09-01 15:57:44 +0200

    fix formula

--- src/hooks/useMaxLeverage.ts
+++ src/hooks/useMaxLeverage.ts
@@ -1,9 +1,11 @@
+import { useTokenBalance } from '@usedapp/core';
 import BigNumber from 'bignumber.js';
 
 import { useRiskFactor } from './useRiskFactor';
-import { useBalance, useVaultData } from './useVault';
+import { useVaultData } from './useVault';
 
 import { StrategyContractType } from '@/global/types';
+import { CORE } from '@/global/constants';
 
 export function useMaxLeverage(
   spentToken: string,
@@ -12,7 +14,7 @@ export function useMaxLeverage(
   strategy: StrategyContractType
 ) {
   const vaultData = useVaultData(spentToken);
-  const balance: BigNumber = useBalance(spentToken);
+  const balance = useTokenBalance(spentToken, CORE.Vault.address);
   const riskFactor: BigNumber = useRiskFactor(strategy, obtainedToken);
   const netLoans: BigNumber = BigNumber(vaultData?.netLoans.toString());
   const baseFee: BigNumber = BigNumber(vaultData?.baseFee.toString());
@@ -24,20 +26,20 @@ export function useMaxLeverage(
     netLoans
       .plus(BigNumber.max(netLoans.minus(insuranceReserveBalance), 0))
       .multipliedBy(riskFactor)
-      .dividedBy(balance)
+      .dividedBy(
+        BigNumber(balance?.toString() || 0)
+          .plus(netLoans)
+          .minus(insuranceReserveBalance)
+      )
   );
-
   const maxLeverage = BigNumber('500').dividedBy(baseIR);
-  console.log('maxLeverage', maxLeverage.toString());
-  console.log(
-    'altro',
-    balance.minus(insuranceReserveBalance).dividedBy(margin).toString()
-  );
 
   return BigNumber.max(
     BigNumber.min(
       maxLeverage,
-      balance.minus(insuranceReserveBalance).dividedBy(margin)
+      BigNumber(balance?.toString() || 0)
+        .minus(insuranceReserveBalance)
+        .dividedBy(margin)
     ),
     1
   ).toNumber();
