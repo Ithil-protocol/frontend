41717306ebd9c68c1364825305ab1bf4245bd455
devneser <zh_alex@yahoo.com>
2022-09-13 20:06:00 +0800

    refactor: update marks

--- src/pages/trade/YearnStrategy.tsx
+++ src/pages/trade/YearnStrategy.tsx
@@ -183,7 +183,7 @@ export default function YearnStrategyPage() {
     if (maxLeverage === 1) return { 1: '1x' };
     const increment = (maxLeverage - 1) / 10;
     for (let i = 1; i <= maxLeverage; i += increment) {
-      marks[i] = `${formatAmount(i, 0, true, 2)}x`;
+      marks[i] = i == 1 ? `${formatAmount(i, 0, true, 2)}x` : ' ';
     }
     if (
       !Object.keys(marks)
@@ -232,7 +232,7 @@ export default function YearnStrategyPage() {
                 tooltipText="The capital boost on the margin invested"
                 min={1}
                 max={maxLeverage}
-                step={0.2}
+                step={0.1}
                 value={Number(leverage.toFixed(1))}
                 onChange={(value) => setLeverage(value as number)}
                 marks={sliderMarks}
