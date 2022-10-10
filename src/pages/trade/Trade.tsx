bf165cd05f9452b5d676752801fbb499823d807d
413x <zh_alex@yahoo.com>
2022-10-11 03:43:41 +0700

    Update Trade UI (#108)
    * feat: update trade UI
    
    * refactor: update file name
    
    * refactor: fix trade table

--- src/pages/trade/Trade.tsx
+++ src/pages/trade/Trade.tsx
@@ -1,28 +1,90 @@
 /** @jsxImportSource @emotion/react */
 import 'twin.macro';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import Page from '@/components/based/Page';
+import DataTable from '@/components/based/table/DataTable';
+import Txt from '@/components/based/Txt';
 import { TRADE_STRATEGIES } from '@/global/constants';
-import { StrategyWidget } from '@/components/composed/trade/StrategyWidget';
+import TradeTableRow from '@/components/composed/trade/TradeTableRow';
+import Tooltip from '@/components/based/Tooltip';
 
 const TradePage = () => {
-  const navigate = useNavigate();
-
   return (
     <Page heading="Trading Strategies">
       <div
-        tw="flex flex-row gap-x-4 flex-wrap gap-y-4 justify-center"
+        tw="laptop:flex laptop:w-full flex-row gap-x-4 flex-wrap gap-y-4 justify-center laptop:min-width[800px] mobile:display[initial] mobile:min-width[initial] mobile:w-full mt-2"
         id="strategies"
       >
-        {TRADE_STRATEGIES.map((strategy) => (
-          <StrategyWidget
-            key={strategy.id}
-            {...strategy}
-            onClick={() => strategy.url && navigate(strategy.url)}
-          />
-        ))}
+        <DataTable
+          head={[
+            {
+              id: 'title',
+              content: (
+                <div tw="flex items-center gap-2 justify-center">
+                  <Txt.Body2Regular tw="text-font-100">
+                    Strategy
+                  </Txt.Body2Regular>
+                </div>
+              ),
+            },
+            {
+              id: 'exposure',
+              content: (
+                <div tw="flex items-center gap-2 justify-center">
+                  <Txt.Body2Regular tw="text-font-100">
+                    Exposure
+                  </Txt.Body2Regular>
+                  <Tooltip text="Exposure" />
+                </div>
+              ),
+            },
+            {
+              id: 'description',
+              content: (
+                <div tw="flex items-center gap-2 justify-center">
+                  <Txt.Body2Regular tw="text-font-100">
+                    Description
+                  </Txt.Body2Regular>
+                </div>
+              ),
+            },
+            {
+              id: 'utilisation_rate',
+              content: (
+                <div tw="flex items-center gap-2 justify-center">
+                  <Txt.Body2Regular tw="text-font-100">
+                    Utilisation Rate
+                  </Txt.Body2Regular>
+                  <Tooltip text="Utilisation Rate" />
+                </div>
+              ),
+            },
+            {
+              id: 'risk_profile',
+              content: (
+                <div tw="flex items-center gap-2 justify-center">
+                  <Txt.Body2Regular tw="text-font-100">
+                    Risk profile
+                  </Txt.Body2Regular>
+                  <Tooltip text="Risk profile" />
+                </div>
+              ),
+            },
+            { id: 'action', content: '' },
+          ]}
+          data={TRADE_STRATEGIES.map((strategy) => ({
+            title: strategy.title,
+            exposure: null,
+            description: strategy.description,
+            utilisation_rate: strategy.uRate,
+            risk_profile: strategy.risk,
+            action: null,
+            url: strategy.url,
+          }))}
+          loading={false}
+          RowComponent={TradeTableRow}
+        />
       </div>
     </Page>
   );
