b0c9a0a323c527388cf2510ba30356fb0fec25bf
devneser <zh_alex@yahoo.com>
2022-10-10 23:30:24 +0800

    feat: add hardhat envs

--- src/config/dapp.ts
+++ src/config/dapp.ts
@@ -1,17 +1,31 @@
-import { ChainId, Config, Goerli } from '@usedapp/core';
+import { ChainId, Config, Goerli, Hardhat } from '@usedapp/core';
 
-import { infuraUrl, alchemyUrl, pollingIntervalProvider } from '@/global/utils';
+import { alchemyUrl, pollingIntervalProvider } from '@/global/utils';
 
 export const POLLING_INTERVAL = 10_000;
 
+export const IS_HARDHAT_SET =
+  process.env.NODE_ENV === 'development' &&
+  process.env.REACT_APP_HARDHAT_ENV === 'true';
+
+const defaulUrls = {
+  [Goerli.chainId]: pollingIntervalProvider(
+    alchemyUrl(Goerli.chainId),
+    POLLING_INTERVAL
+  ),
+};
+
 export const DAPP_CONFIG: Config = {
   readOnlyChainId: ChainId.Goerli,
-  networks: [Goerli],
+  networks: [Goerli].concat(IS_HARDHAT_SET ? [Hardhat] : []),
   readOnlyUrls: {
-    [Goerli.chainId]: pollingIntervalProvider(
-      alchemyUrl(Goerli.chainId),
-      POLLING_INTERVAL
-    ),
+    ...defaulUrls,
+    ...(IS_HARDHAT_SET
+      ? {
+          [Hardhat.chainId]:
+            process.env.REACT_APP_HARDHAT_RPC ?? 'http://localhost:8545',
+        }
+      : {}),
   },
   pollingInterval: POLLING_INTERVAL,
   multicallVersion: 2,
