399e10671190283907ddae736c7dae2334ba3b91
devneser <zh_alex@yahoo.com>
2022-10-10 23:30:24 +0800

    feat: add hardhat envs

--- src/config/connectors.ts
+++ src/config/connectors.ts
@@ -1,18 +1,32 @@
-import { Goerli } from '@usedapp/core';
+import { Goerli, Hardhat } from '@usedapp/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 
-import { infuraUrl, alchemyUrl } from '@/global/utils';
+import { IS_HARDHAT_SET } from './dapp';
 
-const supportedChains = [Goerli.chainId];
+import { alchemyUrl } from '@/global/utils';
+
+const supportedChains = [Goerli.chainId].concat(
+  IS_HARDHAT_SET ? [Hardhat.chainId] : []
+);
 
 export const injected = new InjectedConnector({
   supportedChainIds: supportedChains,
 });
 
+const defaulUrls = {
+  [Goerli.chainId]: alchemyUrl(Goerli.chainId),
+};
+
 export const walletconnect = new WalletConnectConnector({
   rpc: {
-    [Goerli.chainId]: alchemyUrl(Goerli.chainId),
+    ...defaulUrls,
+    ...(IS_HARDHAT_SET
+      ? {
+          [Hardhat.chainId]:
+            process.env.REACT_APP_HARDHAT_RPC ?? 'http://localhost:8545',
+        }
+      : {}),
   },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
