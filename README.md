7a934a8ecc6183b78fdd6c1d20213c3bf90d332f
valerio <valerio.coltre@gmail.com>
2022-11-30 23:31:43 +0100

    ga configuration is more straightforward

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Ithil official frontend
 
+# How to start the project
+
+1. copy `.env.example` to either `.env.development` or `.env.local`
+2. populate required variables
+3. do `yarn`
+4. can now execute any package.json `scripts`
+
 ## Endpoints
 
+- [testnet.ithil.fi](https://testnet.ithil.fi)
 - [app.ithil.fi](https://app.ithil.fi)
 - IPFS coming soon...
--- src/config/ga4.ts
+++ src/config/ga4.ts
@@ -1,19 +1,20 @@
 import ga4 from 'react-ga4'
 import { UaEventOptions } from 'react-ga4/types/ga4'
 
-const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS
+// GA4 id has this format: G-XXXXXXXXXX
+const ga4Id = process.env.REACT_APP_GOOGLE_ANALYTICS
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const init = () => {
-  if (TRACKING_ID == null) return
-
-  ga4.initialize(TRACKING_ID, {
-    testMode: !isProduction
-  })
+  if (isProduction && ga4Id) ga4.initialize(ga4Id)
 }
 
-export const sendEvent = (optionsOrName: UaEventOptions | string, params?: any) => ga4.event(optionsOrName, params)
-export const sendPageview = (path: string) => ga4.send({
-  hitType: 'pageview',
-  page: path
-})
+export const sendEvent = (
+  optionsOrName: UaEventOptions | string,
+  params?: any
+) => ga4.event(optionsOrName, params)
+export const sendPageview = (path: string) =>
+  ga4.send({
+    hitType: 'pageview',
+    page: path,
+  })
