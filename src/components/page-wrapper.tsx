e2c0b1cc3034204cabef417c64bdd83dde0bd60a
colthreepv <valerio.coltre@gmail.com>
2023-03-29 18:56:11 +0200

    small container padding

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -12,7 +12,7 @@ interface PageWrapperProps extends PropsWithClassName {
 const raleway = Raleway({ subsets: ['latin'], variable: '--font-inter' })
 
 const PageWrapper: FC<PageWrapperProps> = ({ children, heading, className }) => (
-  <main className={classNames(['container p-1 sm:p-0 mx-auto font-sans', raleway.variable, className])}>
+  <main className={classNames(['container p-1 sm:p-0 md:p-2 mx-auto font-sans', raleway.variable, className])}>
     <div className="flex flex-col items-center w-full">
       <Heading as="h1" size="h1" className="mb-2">
         {heading}
