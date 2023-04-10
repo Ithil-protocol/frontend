145830af9be75f078c3b673168cc8a7c78502e62
valerio <valerio.coltre@gmail.com>
2023-04-10 14:24:11 +0200

    component handles stupid first nextjs draw

--- src/pages/services/index.page.tsx
+++ src/pages/services/index.page.tsx
@@ -1,80 +1,10 @@
-import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 import Head from 'next/head'
-import { type FC, useState } from 'react'
-import { type Address } from 'wagmi'
+import { type FC } from 'react'
 
 import PageWrapper from '@/components/page-wrapper'
-import { useToken } from '@/hooks/use-token.hook'
-import { useTransactionFeedback } from '@/hooks/use-transaction.hook'
-import { type ServiceAsset } from '@/types/onchain.types'
-import { stringInputToBigNumber } from '@/utils/input.utils'
 
+import { DynamicServiceDeposit } from './single-asset-deposit'
 import { useServices } from './use-services.hook'
-
-interface ServiceDepositProps {
-  asset: ServiceAsset
-  serviceAddress: Address
-}
-
-const ServiceDeposit: FC<ServiceDepositProps> = ({ asset, serviceAddress }) => {
-  const [inputAmount, setInputAmount] = useState<string>('0')
-  const inputBigNumber = stringInputToBigNumber(inputAmount, asset.decimals)
-
-  const { useAllowance, useApprove } = useToken(asset.tokenAddress)
-  const { data: allowance, refetch: refetchAllowance } = useAllowance(asset.tokenAddress, serviceAddress)
-  const {
-    data: approveData,
-    isLoading: isApproveLoading,
-    writeAsync: approve,
-  } = useApprove(serviceAddress, inputBigNumber)
-  const { trackTransaction } = useTransactionFeedback()
-
-  const isButtonDisabled = false
-  const isButtonLoading = false
-  const isApproved = allowance?.gte(inputBigNumber) ?? false
-  const isMaxDisabled = false
-
-  const onInputChange = (amount: string) => {
-    setInputAmount(amount)
-  }
-  const onActionClick = async () => {
-    if (!isApproved) {
-      const result = await approve?.()
-      await trackTransaction(result, `Approve ${inputAmount} ${asset.name}`)
-      await refetchAllowance()
-    }
-  }
-  const onMaxClick = () => {}
-
-  return (
-    <div>
-      <InputGroup size="md">
-        <Input
-          type="number"
-          step="0.1"
-          variant="filled"
-          value={inputAmount}
-          onChange={(event) => onInputChange(event.target.value)}
-        />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={onMaxClick} isDisabled={isMaxDisabled} variant="insideInput">
-            Max
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-      <Button
-        onClick={() => {
-          void onActionClick()
-        }}
-        isDisabled={isButtonDisabled}
-        isLoading={isButtonLoading}
-        loadingText={isButtonLoading ? 'Awaiting' : undefined}
-      >
-        {isApproved ? `Deposit ${asset.name}` : `Approve ${asset.name}`}
-      </Button>
-    </div>
-  )
-}
 
 const Services: FC = () => {
   const { names, services, assetsByService } = useServices()
@@ -94,7 +24,7 @@ const Services: FC = () => {
             <p key={asset}>{asset}</p>
           ))}
         </div>
-        <ServiceDeposit asset={services.AAVE.assets[0]} serviceAddress={services.AAVE.address} />
+        <DynamicServiceDeposit asset={services.AAVE.assets[0]} serviceAddress={services.AAVE.address} />
       </PageWrapper>
     </>
   )
---
+++ src/pages/services/single-asset-deposit.tsx
@@ -0,0 +1,133 @@
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+import { type FC, useState } from 'react'
+import { type Address } from 'wagmi'
+
+import { useToken } from '@/hooks/use-token.hook'
+import { useTransactionFeedback } from '@/hooks/use-transaction.hook'
+import { type ServiceAsset } from '@/types/onchain.types'
+import { abbreviateBigNumber, stringInputToBigNumber } from '@/utils/input.utils'
+
+interface WidgetSingleAssetDepositProps {
+  inputAmount: string
+  onInputChange: (amount: string) => void
+  onActionClick: () => void
+  onMaxClick: () => void
+  isButtonDisabled: boolean
+  isButtonLoading: boolean
+  isMaxDisabled: boolean
+  isApproved: boolean
+  asset?: ServiceAsset
+}
+
+export const WidgetSingleAssetDeposit: FC<WidgetSingleAssetDepositProps> = ({
+  inputAmount,
+  onInputChange,
+  onActionClick,
+  onMaxClick,
+  isButtonDisabled,
+  isButtonLoading,
+  isMaxDisabled,
+  isApproved,
+  asset,
+}) => {
+  return (
+    <div>
+      <InputGroup size="md">
+        <Input
+          type="number"
+          step="0.1"
+          variant="filled"
+          value={inputAmount}
+          onChange={(event) => onInputChange(event.target.value)}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={onMaxClick} isDisabled={isMaxDisabled} variant="insideInput">
+            Max
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <Button
+        onClick={() => {
+          void onActionClick()
+        }}
+        isDisabled={isButtonDisabled}
+        isLoading={isButtonLoading}
+        loadingText={isButtonLoading ? 'Awaiting' : undefined}
+      >
+        {asset == null ? 'Loading...' : isApproved ? 'Open position' : `Approve ${asset.name}`}
+      </Button>
+    </div>
+  )
+}
+
+interface ServiceDepositProps {
+  asset: ServiceAsset
+  serviceAddress: Address
+}
+
+export const ServiceDeposit: FC<ServiceDepositProps> = ({ asset, serviceAddress }) => {
+  const [inputAmount, setInputAmount] = useState<string>('0')
+  const inputBigNumber = stringInputToBigNumber(inputAmount, asset.decimals)
+
+  const { useAllowance, useApprove } = useToken(asset.tokenAddress)
+  const { data: allowance, refetch: refetchAllowance } = useAllowance(asset.tokenAddress, serviceAddress)
+  const {
+    data: approveData,
+    isLoading: isApproveLoading,
+    writeAsync: approve,
+  } = useApprove(serviceAddress, inputBigNumber)
+  const { trackTransaction } = useTransactionFeedback()
+
+  const isButtonDisabled = inputBigNumber.isZero()
+  const isButtonLoading = false
+  const isApproved = allowance?.gte(inputBigNumber) ?? false
+  const isMaxDisabled = false
+
+  const onInputChange = (amount: string) => {
+    setInputAmount(amount)
+  }
+  const onActionClick = async () => {
+    if (!isApproved) {
+      const result = await approve?.()
+      await trackTransaction(result, `Approve ${inputAmount} ${asset.name}`)
+      await refetchAllowance()
+    }
+  }
+  const onMaxClick = () => {}
+
+  console.log({ inputBigNumber: abbreviateBigNumber(inputBigNumber, asset.decimals), isApproved })
+
+  return (
+    <WidgetSingleAssetDeposit
+      inputAmount={inputAmount}
+      onInputChange={onInputChange}
+      onActionClick={onActionClick}
+      onMaxClick={onMaxClick}
+      isButtonDisabled={isButtonDisabled}
+      isButtonLoading={isButtonLoading}
+      isMaxDisabled={isMaxDisabled}
+      isApproved={isApproved}
+      asset={asset}
+    />
+  )
+}
+
+export const DynamicServiceDeposit = dynamic(
+  async () => await import('@/pages/services/single-asset-deposit').then((mod) => mod.ServiceDeposit),
+  {
+    ssr: false,
+    loading: () => (
+      <WidgetSingleAssetDeposit
+        inputAmount="0"
+        onInputChange={() => {}}
+        onActionClick={() => {}}
+        onMaxClick={() => {}}
+        isButtonDisabled={true}
+        isButtonLoading={false}
+        isMaxDisabled={false}
+        isApproved={false}
+      />
+    ),
+  },
+)
