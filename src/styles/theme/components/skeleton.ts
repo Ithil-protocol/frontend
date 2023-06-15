fc3bf3a3b69054770bea833570cc95045c4106a1
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-15 16:37:58 +0330

    fix: skeleton style

--- src/styles/theme/components/skeleton.ts
+++ src/styles/theme/components/skeleton.ts
@@ -1,14 +1,28 @@
-import { StyleConfig } from "@chakra-ui/react";
+import {
+  cssVar,
+  defineStyle,
+  defineStyleConfig,
+} from "@chakra-ui/styled-system";
 
-export const ithilSkeletonStyle: StyleConfig = {
-  baseStyle: {
-    _dark: {
-      "--skeleton-start-color": "#e2e3e3",
-      "--skeleton-end-color": "#f5f5f5",
-    },
-    _light: {
-      "--skeleton-start-color": "#262e45",
-      "--skeleton-end-color": "#48516d",
-    },
+const $startColor = cssVar("skeleton-start-color");
+const $endColor = cssVar("skeleton-end-color");
+
+const baseStyle = defineStyle({
+  _light: {
+    [$startColor.variable]: "#e2e3e3",
+    [$endColor.variable]: "#f5f5f5",
   },
-};
+
+  _dark: {
+    [$startColor.variable]: "#262e45",
+    [$endColor.variable]: "#48516d",
+  },
+  background: $startColor.reference,
+  borderColor: $endColor.reference,
+  opacity: 0.7,
+  borderRadius: "sm",
+});
+
+export const ithilSkeletonStyle = defineStyleConfig({
+  baseStyle,
+});
