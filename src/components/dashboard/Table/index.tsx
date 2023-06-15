7bcc619a03d4764bf0bf85e1758558785dea6781
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-06-15 23:04:47 +0330

    dashboard(feat): add  darkmode and light mode  table

--- src/components/dashboard/Table/index.tsx
+++ src/components/dashboard/Table/index.tsx
@@ -4,23 +4,34 @@ import {
   Th,
   Thead,
   Tr,
+  useColorMode,
 } from "@chakra-ui/react";
 import { FC } from "react";
 
 import { ColumnsActive } from "@/containers/DashboardPage";
+import { mode } from "@/utils/theme";
 
 interface Props {
   columns: ColumnsActive[];
 }
 
 const Table: FC<Props> = ({ columns }) => {
+  const { colorMode } = useColorMode();
   return (
     <TableContainer width="full">
       <DefaultTable variant="unstyled">
         <Thead>
           <Tr>
             {columns.map((col, index) => (
-              <Th key={col + index}>{col}</Th>
+              <Th
+                color={mode(colorMode, "primary.700", "primary.700.dark")}
+                className="font-sans"
+                fontSize="18px"
+                fontWeight="medium"
+                key={col + index}
+              >
+                {col}
+              </Th>
             ))}
           </Tr>
         </Thead>
