26d522082c4a85d7480b4d2dffb4570cf0258f64
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-06-15 12:42:42 +0330

    dashboard(fix): remove extra import

--- src/containers/DashboardPage.tsx
+++ src/containers/DashboardPage.tsx
@@ -1,8 +1,5 @@
-import { Box, Button, ButtonGroup, Text } from "@chakra-ui/react";
-import Link from "next/link";
 import { useState } from "react";
 
-import { ArrowLeft } from "@/assets/svgs";
 import Header from "@/components/dashboard/Header";
 import PageWrapper from "@/components/page-wrapper";
 import { viewTypes } from "@/types";
