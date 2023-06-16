5b9d8e9de29d47956ea5366eb6391b8e301509d3
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-06-16 20:01:35 +0330

    dashbaord(feat): add header in deatil page.

--- src/components/dashboard/detail/Header.tsx
+++ src/components/dashboard/detail/Header.tsx
@@ -1,5 +1,8 @@
-import { GridItem, useColorMode } from "@chakra-ui/react";
+import { Box, GridItem, HStack, Text, useColorMode } from "@chakra-ui/react";
+import Link from "next/link";
 
+import { ArrowLeft } from "@/assets/svgs";
+import TokenIcon from "@/components/TokenIcon";
 import { palette } from "@/styles/theme/palette";
 import { pickColor } from "@/utils/theme";
 
@@ -20,7 +23,57 @@ const Header = () => {
         md: "27px",
       }}
     >
-      Header
+      <HStack
+        spacing={{
+          base: "10px",
+          md: "15px",
+        }}
+      >
+        <Link href="/dashboard">
+          <ArrowLeft
+            className={`${colorMode === "light" && "fill-[#070b0f]"}`}
+          />
+        </Link>
+        <HStack
+          spacing={{
+            base: "10px",
+            md: "15px",
+          }}
+        >
+          <Box position="relative" display="inline-block" mr="50px">
+            <Box
+              position="absolute"
+              left="0"
+              top="50%"
+              transform="translateY(-50%)"
+            >
+              <TokenIcon name="USDT" />
+            </Box>
+            <Box
+              position="absolute"
+              left="15px"
+              top="50%"
+              transform="translateY(-50%)"
+            >
+              <TokenIcon
+                style={{
+                  border:
+                    colorMode === "light"
+                      ? "2.5px solid #f2f5f6"
+                      : "2.5px solid #151a29",
+                  borderRadius: "100%",
+                }}
+                width={42}
+                height={42}
+                name="DAI"
+              />
+            </Box>
+          </Box>
+          <Text fontSize="24px" fontWeight="bold" lineHeight="22px">
+            ETH / BNB
+          </Text>
+        </HStack>
+      </HStack>
     </GridItem>
   );
 };
