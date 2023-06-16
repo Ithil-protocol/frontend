b9c4e57b9844dea3b3d825727598217e9510f455
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-06-16 19:44:04 +0330

    dashboard(feat): add responsiv sizes to detail page

--- src/containers/DashboardDetailPage.tsx
+++ src/containers/DashboardDetailPage.tsx
@@ -9,38 +9,85 @@ const DashboardDetailPage = () => {
   return (
     <PageWrapper>
       <Grid
-        templateAreas={`"header header"
-                      "nav main"
-                     `}
-        templateRows="1fr auto"
-        templateColumns="1fr 1fr"
-        gap={["24px"]}
+        templateAreas={{
+          base: `"header"
+          "nav"
+          "main"
+         `,
+          md: `"header header"
+         "nav main"
+        `,
+        }}
+        templateRows={{
+          base: "1fr",
+          lg: "1fr auto",
+        }}
+        templateColumns={{
+          base: "1fr",
+          md: "1fr 1fr",
+        }}
+        gap={{
+          base: "12px",
+          md: "20px",
+          lg: "24px",
+        }}
         height="full"
         width="full"
       >
         <GridItem
           bg={pickColor(colorMode, palette.colors.primary, "100")}
           area="header"
-          borderRadius={["12px"]}
-          paddingX={["40px"]}
-          paddingY={["27px"]}
+          borderRadius={"12px"}
+          paddingX={{
+            base: "20px",
+            md: "30px",
+            lg: "40px",
+          }}
+          paddingY={{
+            base: "17px",
+            md: "27px",
+          }}
         >
           Header
         </GridItem>
         <GridItem area="nav">
-          <Grid gap={["24px"]} height="full" width="full">
+          <Grid
+            gap={{
+              base: "12px",
+              md: "20px",
+              lg: "24px",
+            }}
+            height="full"
+            width="full"
+          >
             <GridItem
               borderRadius={["12px"]}
-              paddingX={["40px"]}
-              paddingY={["40px"]}
+              paddingX={{
+                base: "20px",
+                md: "30px",
+                lg: "40px",
+              }}
+              paddingY={{
+                base: "20px",
+                md: "30px",
+                lg: "40px",
+              }}
               bg={pickColor(colorMode, palette.colors.primary, "100")}
             >
               Positions Details
             </GridItem>
             <GridItem
               borderRadius={["12px"]}
-              paddingX={["40px"]}
-              paddingY={["40px"]}
+              paddingX={{
+                base: "20px",
+                md: "30px",
+                lg: "40px",
+              }}
+              paddingY={{
+                base: "20px",
+                md: "30px",
+                lg: "40px",
+              }}
               bg={pickColor(colorMode, palette.colors.primary, "100")}
             >
               Close Position
@@ -49,8 +96,16 @@ const DashboardDetailPage = () => {
         </GridItem>
         <GridItem
           borderRadius={["12px"]}
-          paddingX={["40px"]}
-          paddingY={["40px"]}
+          paddingX={{
+            base: "20px",
+            md: "30px",
+            lg: "40px",
+          }}
+          paddingY={{
+            base: "20px",
+            md: "30px",
+            lg: "40px",
+          }}
           bg={pickColor(colorMode, palette.colors.primary, "100")}
           area="main"
         >
