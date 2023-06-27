a18362fbcf71eebbeea732f6fd192ad9e7a2b6bc
arian.hamdi <arian.hamdi@gmail.com>
2023-06-27 22:10:49 +0330

    connect vault page to the hooks

--- src/hooks/useVaultDetails.ts
+++ src/hooks/useVaultDetails.ts
@@ -5,28 +5,36 @@ import { vaultContracts } from "@/contracts";
 export const useVaultDetails = (vault: string) => {
   const vaultContract = vaultContracts[vault.toUpperCase()];
 
-  return useContractReads({
-    contracts: [
-      {
-        ...vaultContract,
-        functionName: "currentProfits",
-      },
-      {
-        ...vaultContract,
-        functionName: "currentLosses",
-      },
-      {
-        ...vaultContract,
-        functionName: "netLoans",
-      },
-      {
-        ...vaultContract,
-        functionName: "latestRepay",
-      },
-      {
-        ...vaultContract,
-        functionName: "freeLiquidity",
-      },
-    ],
+  const contracts = [
+    {
+      ...vaultContract,
+      functionName: "currentProfits",
+    },
+    {
+      ...vaultContract,
+      functionName: "currentLosses",
+    },
+    {
+      ...vaultContract,
+      functionName: "netLoans",
+    },
+    {
+      ...vaultContract,
+      functionName: "latestRepay",
+    },
+  ];
+
+  const result = useContractReads({
+    contracts,
   });
+
+  const data: any = {};
+  contracts.forEach(({ functionName }, index) => {
+    data[functionName] = result?.data?.[index]?.result;
+  });
+
+  return {
+    ...result,
+    data,
+  };
 };
