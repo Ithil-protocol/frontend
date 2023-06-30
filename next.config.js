786209c0195fedf9252556ad844dbf142958608a
Arian Hamdi <arian.hamdi@gmail.com>
2023-06-30 20:46:42 +0330

    fix trailing slash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
