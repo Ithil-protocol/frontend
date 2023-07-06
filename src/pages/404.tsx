00b2aa6124ae5126a2e2ee11d8ad0793244dd24c
Arian Hamdi <arian.hamdi@gmail.com>
2023-07-06 03:59:53 +0330

    feat: add navigation to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Text, useColorMode } from "@chakra-ui/react";
 import Link from "next/link";
 
 import { IthilWizard } from "@/assets/svgs";
+import PageWrapper from "@/components/page-wrapper";
 import { palette } from "@/styles/theme/palette";
 import { mode } from "@/utils/theme";
 
@@ -9,50 +10,52 @@ const Page404 = () => {
   const { colorMode } = useColorMode();
 
   return (
-    <Box
-      style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    <PageWrapper>
       <Box
         style={{
-          padding: "20px",
-          borderRadius: "10px",
           width: "100%",
           display: "flex",
-          maxWidth: "400px",
           justifyContent: "center",
           alignItems: "center",
-          flexDirection: "column",
+          height: "80vh",
         }}
       >
-        <Text
+        <Box
           style={{
-            fontSize: "28px",
+            padding: "20px",
+            borderRadius: "10px",
+            width: "100%",
+            display: "flex",
+            maxWidth: "400px",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
           }}
         >
-          Page not found!
-        </Text>
-
-        <IthilWizard width="100%" height="400px" />
-
-        <Link style={{ width: "100%" }} href="/lend">
-          <Button
-            bg={mode(
-              colorMode,
-              palette.variants.primary.action,
-              palette.variants.primary["action.dark"]
-            )}
+          <Text
+            style={{
+              fontSize: "28px",
+            }}
           >
-            Back to Home!
-          </Button>
-        </Link>
+            Page not found!
+          </Text>
+
+          <IthilWizard width="100%" height="400px" />
+
+          <Link style={{ width: "100%" }} href="/lend">
+            <Button
+              bg={mode(
+                colorMode,
+                palette.variants.primary.action,
+                palette.variants.primary["action.dark"]
+              )}
+            >
+              Back to Home!
+            </Button>
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </PageWrapper>
   );
 };
 
