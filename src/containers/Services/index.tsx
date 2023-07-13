a7d0ea291ee657abe5509f4b580e22a7461cf282
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-07-13 11:06:20 +0330

    feat: refactor  ServiceCard

--- src/containers/Services/index.tsx
+++ src/containers/Services/index.tsx
@@ -1,104 +1,10 @@
-import {
-  Box,
-  Button,
-  Heading,
-  Text,
-  VStack,
-  useColorMode,
-} from "@chakra-ui/react";
-import { Icon } from "@iconify/react";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { type FC } from "react";
 
 import services from "@/data/services.json";
-import { palette } from "@/styles/theme/palette";
-import { Service, ServiceType } from "@/types";
-import { pickColor } from "@/utils/theme";
+import { Service } from "@/types";
 
-import ServiceIcon from "../Service/ServiceIcon";
-
-const ServiceToken: FC<{ token: string }> = ({ token }) => (
-  <div className="flex py-1 min-w-[92px] border border-primary-500 rounded-md">
-    <Text textStyle="slender-sm" fontWeight="normal" className="mx-auto">
-      {token.toUpperCase()}
-    </Text>
-  </div>
-);
-
-interface ServiceCardProps {
-  assets: string[];
-  description: string | ((assets: string[]) => string);
-  to: string;
-  multiplier: string;
-  name: string;
-  apy: string;
-  tvl: string;
-}
-
-const ServiceCard: FC<ServiceCardProps> = ({
-  assets,
-  description,
-  to,
-  multiplier,
-  name,
-  apy,
-  tvl,
-}) => {
-  const { colorMode } = useColorMode();
-  const { push } = useRouter();
-  return (
-    <Box className="flex flex-col p-7 rounded-xl bg-primary-100">
-      <div className="flex justify-between mb-6">
-        <ServiceIcon name={name as ServiceType} />
-        {/* 1 - 10% multiplier */}
-        <div className="flex items-center gap-1 px-2 py-1 border rounded-md border-primary-500">
-          <Icon
-            icon="ph:lightning-fill"
-            color={colorMode === "dark" ? "white" : "black"}
-          ></Icon>
-          <Text textStyle="slender-sm">{multiplier}</Text>
-        </div>
-      </div>
-      <Heading size="h3" className="mb-6">
-        {name}
-      </Heading>
-      <div className="flex py-3 mb-4 rounded-md bg-primary-600">
-        <div className="flex items-center gap-2 mx-auto">
-          <Text textStyle="md2">Best APY</Text>
-          <Text textStyle="slender-md">{apy}</Text>
-        </div>
-      </div>
-      <Text className="mb-4">
-        {typeof description === "string" ? description : description(assets)}
-      </Text>
-      <VStack align="start">
-        <div className="flex gap-2 mb-4">
-          <Text
-            textStyle="sm"
-            color={pickColor(colorMode, palette.colors.primary, "700")}
-          >
-            TVL:
-          </Text>
-          <Text textStyle="slender-sm2">{tvl}</Text>
-        </div>
-        {/* tokens array */}
-        <div className="flex flex-wrap gap-2 mb-6 justify-evenly">
-          {assets.map((token) => (
-            <ServiceToken token={token} key={token} />
-          ))}
-        </div>
-      </VStack>
-      <Button
-        onClick={() => push(`/services/${to}`)}
-        size="lg"
-        className="w-full"
-      >
-        Enter
-      </Button>
-    </Box>
-  );
-};
+import ServiceCard from "./ServiceCard";
 
 const ServicesGrid: FC<{ services: Service[] }> = ({ services }) => {
   return (
