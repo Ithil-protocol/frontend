8f78b2661448d7ab4b6952415e3b13a011288b20
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-14 22:05:49 +0330

    feat: refactor token modal

--- src/pages/services/[service]/index.tsx
+++ src/pages/services/[service]/index.tsx
@@ -1,16 +1,9 @@
-import { useRouter } from "next/router";
-
 import TokenModal from "@/components/TokenModal";
 
 const ServiceAsset = () => {
-  const router = useRouter();
-
-  const handleSelectToken = (tokenName: string) => {
-    router.push(`/services/${router.query.service}/${tokenName}`);
-  };
   return (
     <>
-      <TokenModal isOpen onSelectToken={handleSelectToken} />
+      <TokenModal isOpen />
     </>
   );
 };
