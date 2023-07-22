13a0d9a39f20247f43d26d6d2cce87bc056a2f7d
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-07-22 19:35:23 +0330

    feat: add remove booat apy

--- src/containers/Service/FixedYield/index.tsx
+++ src/containers/Service/FixedYield/index.tsx
@@ -32,7 +32,6 @@ const FixedYield = () => {
           description={service.explanation}
           address={"0x9F1C69E1874d44Ad4ce79079C0b7Bd35E7882Ba80"}
           baseApy={baseApy}
-          boostApy={service.boostApy}
           isLoading={isLoading}
         />
         <SafetyScore
