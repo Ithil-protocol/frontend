1ff9cd40de3e57aaaa99827683d5d2febdff6cc8
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-07-27 20:09:11 +0330

    feat: render tokens options from props

--- src/contexts/TokenModal.tsx
+++ src/contexts/TokenModal.tsx
@@ -26,7 +26,6 @@ import React from "react";
 
 import { CloseButton } from "@/assets/svgs";
 import TokenIcon from "@/components/TokenIcon";
-import { placeHolderVaultData, useVaults } from "@/hooks/use-vaults.hook";
 import { useColorMode } from "@/hooks/useColorMode";
 import { VoidNoArgs } from "@/types";
 import { CloseDialogFn, OpenTokenDialogFn, TokenModalOptions } from "@/types";
@@ -36,11 +35,13 @@ const TokenModalContext = createContext<{
   onSelectToken: CloseDialogFn;
   openDialog: OpenTokenDialogFn;
   setOptions: (o: TokenModalOptions) => void;
+  tokens: any[];
 }>({
   closeDialog: () => {},
   onSelectToken: () => {},
   openDialog: () => {},
   setOptions: () => {},
+  tokens: [],
 });
 
 export const useTokenModal = (
@@ -64,7 +65,7 @@ export const getDefaultOptions = (): TokenModalOptions => ({
   isClosable: true,
   onSelectTokenCallback: () => undefined,
   returnPath: "",
-  excludes: [],
+  tokens: [],
 });
 
 interface Props {
@@ -73,7 +74,7 @@ interface Props {
   onSelectToken?: () => void;
   serviceName: string;
   returnPath?: string;
-  excludes: string[];
+  tokens: any[];
 }
 
 const TokenModalComponent: React.FC<Props> = ({
@@ -82,17 +83,14 @@ const TokenModalComponent: React.FC<Props> = ({
   onSelectToken,
   returnPath,
   serviceName,
-  excludes,
+  tokens,
 }) => {
   const { mode } = useColorMode();
 
   const handleClose = () => {
     if (onClose) onClose();
   };
-
-  const { data: vaultData } = useVaults();
-  const vaultDataWithFallback = vaultData ?? placeHolderVaultData;
-  console.log(excludes);
+  console.log("Tokens", tokens);
 
   return (
     <>
@@ -138,80 +136,55 @@ const TokenModalComponent: React.FC<Props> = ({
             }}
           >
             <List p="10px" bg="transparent">
-              {vaultDataWithFallback
-                .filter((item) => !excludes.includes(item.token.name))
-                .map((item) => {
-                  const descriptions = {
-                    USDC: "USD Coin",
-                    USDT: "USD Tether",
-                    WETH: "Wrapped Ether",
-                    WBTC: "Wrapped BTC",
-                  };
+              {tokens.map((item, key) => (
+                <React.Fragment key={key}>
+                  <Link
+                    onClick={onSelectToken}
+                    href={`/services/${serviceName}/${item.name.toLowerCase()}`}
+                  >
+                    <ListItem>
+                      <Button
+                        style={{
+                          border: "0px",
+                          display: "flex",
+                          gap: "15px",
+                          justifyContent: "flex-start",
+                          padding: "30px",
+                          width: "100%",
+                        }}
+                        variant="outline"
+                      >
+                        <div>
+                          <TokenIcon width={40} height={40} name={item.name} />
+                        </div>
 
-                  return {
-                    ...item.token,
-                    description:
-                      descriptions[
-                        item.token.name as keyof typeof descriptions
-                      ],
-                  };
-                })
-                .map((item, key) => (
-                  <React.Fragment key={key}>
-                    <Link
-                      onClick={onSelectToken}
-                      href={`/services/${serviceName}/${item.name.toLowerCase()}`}
-                    >
-                      <ListItem>
-                        <Button
+                        <div
                           style={{
-                            border: "0px",
+                            alignItems: "flex-start",
                             display: "flex",
-                            gap: "15px",
+                            flexDirection: "column",
                             justifyContent: "flex-start",
-                            padding: "30px",
-                            width: "100%",
                           }}
-                          variant="outline"
                         >
-                          <div>
-                            <TokenIcon
-                              width={40}
-                              height={40}
-                              name={item.name}
-                            />
-                          </div>
-
-                          <div
-                            style={{
-                              alignItems: "flex-start",
-                              display: "flex",
-                              flexDirection: "column",
-                              justifyContent: "flex-start",
-                            }}
+                          <Text
+                            fontWeight="medium"
+                            color={mode("secondary.100", "secondary.100.dark")}
                           >
-                            <Text
-                              fontWeight="medium"
-                              color={mode(
-                                "secondary.100",
-                                "secondary.100.dark"
-                              )}
-                            >
-                              {item.name}
-                            </Text>
-                            <Text
-                              fontWeight="medium"
-                              fontSize="md"
-                              color={mode("primary.400.dark", "primary.400")}
-                            >
-                              {item.description}
-                            </Text>
-                          </div>
-                        </Button>
-                      </ListItem>
-                    </Link>
-                  </React.Fragment>
-                ))}
+                            {item.name}
+                          </Text>
+                          <Text
+                            fontWeight="medium"
+                            fontSize="md"
+                            color={mode("primary.400.dark", "primary.400")}
+                          >
+                            {item.description}
+                          </Text>
+                        </div>
+                      </Button>
+                    </ListItem>
+                  </Link>
+                </React.Fragment>
+              ))}
             </List>
           </ModalBody>
 
@@ -236,9 +209,9 @@ const TokenModalProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [options, setOptions] = useState<TokenModalOptions>(getDefaultOptions);
   const [serviceName, setServiceName] = useState("aave");
-  const handleOpen: OpenTokenDialogFn = (sn = serviceName, excludes = []) => {
+  const handleOpen: OpenTokenDialogFn = (tokens, sn = serviceName) => {
     setServiceName(sn);
-    setOptions((prev) => ({ ...prev, excludes }));
+    setOptions((prev) => ({ ...prev, tokens }));
     onOpen();
   };
 
@@ -261,6 +234,7 @@ const TokenModalProvider: React.FC<PropsWithChildren> = ({ children }) => {
         openDialog: handleOpen,
         setOptions,
         onSelectToken: handleSelectToken,
+        tokens: options.tokens,
       }}
     >
       <TokenModalComponent
@@ -269,7 +243,7 @@ const TokenModalProvider: React.FC<PropsWithChildren> = ({ children }) => {
         onClose={handleClose}
         onSelectToken={handleSelectToken}
         returnPath={options.returnPath}
-        excludes={options.excludes}
+        tokens={options.tokens}
       />
 
       {children}
