e5a8331ac65861e3493efcd31f3c94ba4639b7d4
Arian Hamdi <arian.hamdi@gmail.com>
2023-07-31 13:09:22 +0330

    fix: fix service label

--- src/containers/Service/Aave/index.tsx
+++ src/containers/Service/Aave/index.tsx
@@ -28,9 +28,7 @@ const Aave = () => {
   return (
     <Box className="grid w-full grid-cols-10 gap-6">
       <Box className="flex flex-col flex-grow gap-6 col-span-full lg:col-span-7">
-        <ServiceHeading
-          data={{ name: service.name, description: service.description }}
-        />
+        <ServiceHeading service={service} />
         <Graph data={chartData} />
         {/* <StrategyDescription
           description={service.explanation}
--- src/containers/Service/ServiceHeading.tsx
+++ src/containers/Service/ServiceHeading.tsx
@@ -1,30 +1,27 @@
 import { Box, HStack, Heading, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
-import { ServiceName } from "@/types";
+import { Service, ServiceName } from "@/types";
 
 import ServiceIcon from "./ServiceIcon";
 
 interface Props {
-  data: {
-    name: string;
-    description?: string;
-  };
+  service: Service;
 }
 
-const ServiceHeading: FC<Props> = ({ data }) => {
+const ServiceHeading: FC<Props> = ({ service }) => {
   return (
     <div className="flex flex-row gap-2 p-5 rounded-xl bg-primary-100">
       <div className="flex flex-col w-full gap-3">
         <HStack className="flex justify-between w-full mb-6">
           <Heading size="h1b" lineHeight="32px">
-            {data.name}
+            {service.label}
           </Heading>
           <Box className="relative flex justify-end">
-            <ServiceIcon name={data.name as ServiceName} width="50" />
+            <ServiceIcon name={service.name as ServiceName} width="50" />
           </Box>
         </HStack>
-        <Text textStyle="sm">{data.description}</Text>
+        <Text textStyle="sm">{service.description}</Text>
       </div>
     </div>
   );
