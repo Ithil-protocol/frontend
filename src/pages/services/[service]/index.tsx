84441b2a9cc939450b983d634efdc20da298461d
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-31 05:36:43 +0330

    fix: services index page when `serviceName` not found

--- src/pages/services/[service]/index.tsx
+++ src/pages/services/[service]/index.tsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 
 import { useTokenModal } from "@/contexts/TokenModal";
 import { services } from "@/data/services";
+import { useServiceName } from "@/hooks/useServiceName";
+import { getServiceByName } from "@/utils";
 
 const Service = () => {
   const tokenModal = useTokenModal({
@@ -11,20 +13,25 @@ const Service = () => {
   });
   const router = useRouter();
 
-  const serviceName = router.query.service as string;
+  const serviceName = useServiceName();
 
   useEffect(() => {
-    if (!serviceName) return;
+    if (!serviceName) {
+      pushTo404();
+      return;
+    }
 
     const isServiceHasIndexPage = services.some(
       (i) => i.url === `/${serviceName}` && i.hasIndex
     );
 
-    const { tokens = [] } = services.find((i) => i.name === serviceName) || {};
+    const { tokens = [] } = getServiceByName(serviceName);
 
     if (isServiceHasIndexPage) tokenModal.openDialog(tokens, serviceName);
-    else router.push("/404");
+    else pushTo404();
   }, [router, serviceName]);
+
+  const pushTo404 = () => router.push("/404");
 
   return <></>;
 };
