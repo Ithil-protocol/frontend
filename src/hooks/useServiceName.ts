85d3c92b9583340fd44de02c0186bdefd295973a
Arian Hamdi <arian.hamdi@gmail.com>
2023-08-01 06:44:10 +0330

    fix: simplify useServiceName hook

--- src/hooks/useServiceName.ts
+++ src/hooks/useServiceName.ts
@@ -1,33 +1,10 @@
 import { useRouter } from "next/router";
 
 import { ServiceName } from "@/types";
-import { getServiceNames, getSingleQueryParam } from "@/utils";
 
 export const useServiceName = () => {
-  const router = useRouter();
+  const { asPath } = useRouter();
+  const serviceName = asPath.split("/")[2] as ServiceName | undefined;
 
-  const {
-    query: { service: serviceName },
-  } = router;
-
-  const normalizedServiceName = getSingleQueryParam(serviceName);
-
-  const getServiceNameByUrl = (): ServiceName | undefined => {
-    if (typeof window === "undefined") return undefined;
-
-    const queries = history.state.as.split("/").filter(Boolean);
-
-    for (const query of queries) {
-      if (isQueryExistInServiceNames(query)) return query;
-    }
-
-    return undefined;
-  };
-
-  const isQueryExistInServiceNames = (query: string) =>
-    getServiceNames().includes(query as ServiceName);
-
-  return isQueryExistInServiceNames(normalizedServiceName)
-    ? (normalizedServiceName as ServiceName)
-    : getServiceNameByUrl();
+  return serviceName;
 };
