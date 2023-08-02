ebac5935098d0e96450fc4e889c366d9aa32bd74
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2023-08-02 13:50:26 +0330

    feat: add dashboard modal

---
+++ src/containers/Dashboard/Modal.tsx
@@ -0,0 +1,127 @@
+import {
+  Button,
+  Modal as ChakraModal,
+  CloseButton,
+  Divider,
+  HStack,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FC } from "react";
+
+import Slider from "@/components/Slider";
+import { useColorMode } from "@/hooks/useColorMode";
+import { VoidNoArgs } from "@/types";
+
+import ModalItem from "./ModalItem";
+
+interface Data {
+  pnl?: string;
+  margin?: string;
+  amount?: string;
+  service: string | undefined;
+}
+
+interface Props {
+  slider: number;
+  data: Data;
+  isOpen: boolean;
+  onOpen: VoidNoArgs;
+  onClose: VoidNoArgs;
+}
+
+const Modal: FC<Props> = ({ slider, data, isOpen, onClose, onOpen }) => {
+  const { mode } = useColorMode();
+  const handelConfirmBtn = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    e.preventDefault();
+    // console.log("data.quote", data.quote);
+    // if (data.isPnlLoading) return;
+    // const initialQuote = data?.quote || 0n;
+    // const qoutes: Record<string, bigint> = {
+    //   AAVE: (initialQuote * 999n) / 1000n,
+    //   GMX: (initialQuote * 9n) / 10n,
+    // };
+    // const quote = qoutes[data?.type] || 0n;
+    // const result = await close({
+    //   args: [
+    //     data.id,
+    //     encodeAbiParameters(parseAbiParameters("uint256"), [quote]),
+    //   ],
+    // });
+    // await trackTransaction(result, "Position closed");
+    // queryClient.resetQueries();
+  };
+  return (
+    <ChakraModal onClose={onClose} isCentered isOpen={isOpen}>
+      <ModalOverlay backdropFilter="blur(10px)" />
+      <ModalContent
+        display="flex"
+        flexDirection="column"
+        gap="10px"
+        bg={mode("primary.100", "primary.100.dark")}
+      >
+        <ModalHeader
+          style={{
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <span>Close position</span>
+          <Text
+            style={{
+              cursor: "pointer",
+              borderRadius: "8px",
+              padding: "1px",
+            }}
+            onClick={() => onClose()}
+          >
+            <CloseButton width={7} height={7} />
+          </Text>
+        </ModalHeader>
+
+        <Divider
+          style={{
+            width: "95%",
+            alignSelf: "center",
+          }}
+        />
+
+        <ModalBody marginBottom={8}>
+          <VStack spacing="25px" marginTop={4} alignItems="start">
+            <ModalItem title="Pnl" value={data.pnl} />
+            <ModalItem title="Amount" value={data.amount} />
+            <ModalItem title="Margin" value={data.margin} />
+            <ModalItem title="Service" value={data.service} />
+
+            {slider && <Slider value={slider} onChange={undefined} />}
+          </VStack>
+        </ModalBody>
+        <ModalFooter justifyContent="center">
+          <HStack>
+            <Button variant="outline" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              variant="outline"
+              color="#f35959"
+              onClick={handelConfirmBtn}
+            >
+              Confirm
+            </Button>
+          </HStack>
+        </ModalFooter>
+      </ModalContent>
+    </ChakraModal>
+  );
+};
+
+export default Modal;
--- src/containers/Dashboard/Table/ActiveTRow.tsx
+++ src/containers/Dashboard/Table/ActiveTRow.tsx
@@ -1,6 +1,13 @@
-import { Box, Button, HStack, Td, Text, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Td,
+  Text,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { FC } from "react";
-import { encodeAbiParameters, parseAbiParameters } from "viem";
 import { useContractWrite } from "wagmi";
 
 import { aaveABI, callOptionABI, fixedYieldABI, gmxABI } from "@/abi";
@@ -12,10 +19,11 @@ import { gmxAddress } from "@/hooks/generated/gmx";
 import { useTransactionFeedback } from "@/hooks/use-transaction.hook";
 import { useColorMode } from "@/hooks/useColorMode";
 import { useIsMounted } from "@/hooks/useIsMounted";
-import { queryClient } from "@/lib/react-query";
 import { palette } from "@/styles/theme/palette";
-import { TRowTypes } from "@/types";
-import { getAssetByAddress } from "@/utils";
+import { TRowTypes, VaultName } from "@/types";
+import { formatToken, getAssetByAddress } from "@/utils";
+
+import Modal from "../Modal";
 
 interface Data extends Omit<TRowTypes, "createdAt"> {
   pnlPercentage?: string;
@@ -33,6 +41,7 @@ interface Props {
 
 const ActiveTRow: FC<Props> = ({ data }) => {
   const { colorMode, mode, pickColor } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { trackTransaction } = useTransactionFeedback();
 
@@ -70,22 +79,7 @@ const ActiveTRow: FC<Props> = ({ data }) => {
   ) => {
     e.stopPropagation();
     e.preventDefault();
-    console.log("data.quote", data.quote);
-    if (data.isPnlLoading) return;
-    const initialQuote = data?.quote || 0n;
-    const qoutes: Record<string, bigint> = {
-      AAVE: (initialQuote * 999n) / 1000n,
-      GMX: (initialQuote * 9n) / 10n,
-    };
-    const quote = qoutes[data?.type] || 0n;
-    const result = await close({
-      args: [
-        data.id,
-        encodeAbiParameters(parseAbiParameters("uint256"), [quote]),
-      ],
-    });
-    await trackTransaction(result, "Position closed");
-    queryClient.resetQueries();
+    onOpen();
   };
   const asset = getAssetByAddress(data.token);
 
@@ -94,110 +88,127 @@ const ActiveTRow: FC<Props> = ({ data }) => {
   const isPnlPositive = data.pnl ? +data.pnl >= 0 : true;
 
   return (
-    <Tr
-      width="72"
-      bgColor={pickColor(palette.colors.primary, "100")}
-      borderRadius="12px"
-      // onClick={() => router.push("/dashboard/detail/1")}
-      // cursor="pointer"
-      sx={{
-        "& > td": {
-          padding: ["20px 40px", "30px 40px"],
-        },
-        minWidth: "100px",
-        minHeight: "200px",
-      }}
-    >
-      <Td color={mode("primary.main.dark", "primary.main")}>
-        <HStack spacing="20px" alignItems="center">
-          <Box>
-            <HStack>
-              <Box position="relative" display="inline-block" mr="50px">
-                <Box
-                  position="absolute"
-                  left="0"
-                  top="50%"
-                  transform="translateY(-50%)"
-                >
-                  <TokenIcon
-                    style={{
-                      border:
-                        colorMode === "light"
-                          ? "2.5px solid #f2f5f6"
-                          : "2.5px solid #151a29",
-                      borderRadius: "100%",
-                    }}
-                    width={42}
-                    height={42}
-                    name={asset?.name || ""}
-                  />
+    <>
+      <Modal
+        slider={4}
+        data={{
+          pnl: data.formattedPnl,
+          amount: formatToken(
+            getAssetByAddress(data.token)?.name as VaultName,
+            data.amount
+          ),
+          margin: data.margin.toString(),
+          service: data.type,
+        }}
+        onClose={onClose}
+        onOpen={onOpen}
+        isOpen={isOpen}
+      />
+      <Tr
+        width="72"
+        bgColor={pickColor(palette.colors.primary, "100")}
+        borderRadius="12px"
+        // onClick={() => router.push("/dashboard/detail/1")}
+        // cursor="pointer"
+        sx={{
+          "& > td": {
+            padding: ["20px 40px", "30px 40px"],
+          },
+          minWidth: "100px",
+          minHeight: "200px",
+        }}
+      >
+        <Td color={mode("primary.main.dark", "primary.main")}>
+          <HStack spacing="20px" alignItems="center">
+            <Box>
+              <HStack>
+                <Box position="relative" display="inline-block" mr="50px">
+                  <Box
+                    position="absolute"
+                    left="0"
+                    top="50%"
+                    transform="translateY(-50%)"
+                  >
+                    <TokenIcon
+                      style={{
+                        border:
+                          colorMode === "light"
+                            ? "2.5px solid #f2f5f6"
+                            : "2.5px solid #151a29",
+                        borderRadius: "100%",
+                      }}
+                      width={42}
+                      height={42}
+                      name={asset?.name || ""}
+                    />
+                  </Box>
                 </Box>
-              </Box>
-              <Text fontSize="22px" lineHeight="22px">
-                {asset?.name}
-              </Text>
-            </HStack>
-          </Box>
-        </HStack>
-      </Td>
-      <Td
-        color={mode("primary.700", "primary.700.dark")}
-        fontWeight="medium"
-        fontSize="22px"
-        lineHeight="22px"
-      >
-        {data.type}
-      </Td>
-      <Td>
-        {!data.isPnlLoading ? (
-          data?.formattedPnl && (
-            <HStack>
-              <Text
-                fontWeight="medium"
-                color={isPnlPositive ? "#15ac89" : "#f35959"}
-                fontSize="22px"
-                lineHeight="22px"
-              >
-                {data.formattedPnl}
-              </Text>
-              <Text
-                bg={isPnlPositive ? "#15ac89" : "#f35959"}
-                borderRadius="8px"
-                fontWeight="bold"
-                fontFamily="18px"
-                lineHeight="24px"
-                textColor={mode("primary.100", "primary.100.dark")}
-                paddingX="8px"
-                paddingY="4px"
-                fontSize="18px"
-              >
-                {data.pnlPercentage} %
-              </Text>
-            </HStack>
-          )
-        ) : (
-          <Loading />
-        )}
-      </Td>
-      <Td
-        color={mode("primary.700", "primary.700.dark")}
-        fontWeight="medium"
-        fontSize="22px"
-        lineHeight="22px"
-      >
-        {data.margin}
-      </Td>
-      <Td textAlign="end" width={200} height="108px">
-        <Button
-          onClick={handelCancelBtn}
-          variant="outline"
-          color="#f35959"
-          isLoading={isLoading}
+                <Text fontSize="22px" lineHeight="22px">
+                  {asset?.name}
+                </Text>
+              </HStack>
+            </Box>
+          </HStack>
+        </Td>
+        <Td
+          color={mode("primary.700", "primary.700.dark")}
+          fontWeight="medium"
+          fontSize="22px"
+          lineHeight="22px"
         >
-          Close
-        </Button>
-      </Td>
-    </Tr>
+          {data.type}
+        </Td>
+        <Td>
+          {!data.isPnlLoading ? (
+            data?.formattedPnl && (
+              <HStack>
+                <Text
+                  fontWeight="medium"
+                  color={isPnlPositive ? "#15ac89" : "#f35959"}
+                  fontSize="22px"
+                  lineHeight="22px"
+                >
+                  {data.formattedPnl}
+                </Text>
+                <Text
+                  bg={isPnlPositive ? "#15ac89" : "#f35959"}
+                  borderRadius="8px"
+                  fontWeight="bold"
+                  fontFamily="18px"
+                  lineHeight="24px"
+                  textColor={mode("primary.100", "primary.100.dark")}
+                  paddingX="8px"
+                  paddingY="4px"
+                  fontSize="18px"
+                >
+                  {data.pnlPercentage} %
+                </Text>
+              </HStack>
+            )
+          ) : (
+            <Loading />
+          )}
+        </Td>
+        <Td
+          color={mode("primary.700", "primary.700.dark")}
+          fontWeight="medium"
+          fontSize="22px"
+          lineHeight="22px"
+        >
+          {data.margin}
+        </Td>
+        <Td textAlign="end" width={200} height="108px">
+          <Button
+            onClick={handelCancelBtn}
+            variant="outline"
+            color="#f35959"
+            isLoading={isLoading}
+          >
+            Close
+          </Button>
+        </Td>
+      </Tr>
+    </>
   );
 };
 
