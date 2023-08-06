c240744e3b60e4ed95cadea01ca5a16f4c5a7960
dev-aly3n <dev.aly3n@gmail.com>
2023-08-07 02:39:55 +0330

    fix

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,27 @@
 import { testNetwork } from "@/config/chains";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { Chain, configureChains, createConfig } from "wagmi";
+import { Chain, ChainProviderFn, configureChains, createConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 import { queryClient } from "@/lib/react-query";
 import { arbitrum } from "wagmi/chains";
+
+const providersObj : Record<string,ChainProviderFn<Chain>[]>= {
+  mainnet:[
+    alchemyProvider({
+      apiKey:process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!
+    }),
+    publicProvider()
+  ],
+  testnet:[
+    jsonRpcProvider({
+      rpc: (_chain) => ({ http: testNetwork.rpcUrls.default.http[0] }),
+    }),
+  ]
+}
+
+const providers = providersObj[process.env.NEXT_PUBLIC_NETWORK!]
 
 const networks : Record<string,Chain> = {
   mainnet:arbitrum,
@@ -14,11 +32,7 @@ const network = networks[process.env.NEXT_PUBLIC_NETWORK!];
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [network], // until Ithil is not a multi-chain app, we can use only one network
-  [
-    jsonRpcProvider({
-      rpc: (_chain) => ({ http: testNetwork.rpcUrls.default.http[0] }),
-    }),
-  ]
+  providers
 );
 const { connectors } = getDefaultWallets({
   appName: "Ithil Core",
