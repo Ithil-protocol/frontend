c2229633a0d672eafe299a33bf1f6b79d6dcf774
Arian Hamdi <arian.hamdi@gmail.com>
2023-08-06 07:20:56 +0330

    fix: import ithil address from contracts json

--- src/data/ithil-token.ts
+++ src/data/ithil-token.ts
@@ -1,9 +1,11 @@
 import { Address } from "viem";
 
+import contracts from "@/deploy/contracts.json";
+
 export const ithil = {
   name: "ITHIL",
   coingeckoId: "ithil",
   iconName: "ithil",
   decimals: 18,
-  tokenAddress: "0x32a78c6F3bD1D4D1A50830529d1339ce081EDf28" as Address,
+  tokenAddress: contracts.ithil as Address,
 };
