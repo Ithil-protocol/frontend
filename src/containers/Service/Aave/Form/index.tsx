44a71d0d53e07d09620ff33e417da049f8f35a71
dev-aly3n <dev.aly3n@gmail.com>
2023-08-23 21:17:43 +0330

    fix best leverage

--- src/containers/Service/Aave/Form/index.tsx
+++ src/containers/Service/Aave/Form/index.tsx
@@ -171,7 +171,7 @@ const Form = ({ asset }: { asset: Asset }) => {
     },
     {
       label: "Best Leverage:",
-      value: bestLeverage,
+      value: Number(bestLeverage) + 1,
       extension: "x",
       isLoading: isBestLeverageLoading,
     },
