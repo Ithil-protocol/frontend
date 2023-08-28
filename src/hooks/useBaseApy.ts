eecd50de718251e7e7c04ac2b8420c5fdd4960ad
dev-aly3n <dev.aly3n@gmail.com>
2023-08-28 13:10:05 +0330

    test

--- src/hooks/useBaseApy.ts
+++ src/hooks/useBaseApy.ts
@@ -3,6 +3,7 @@ import { useChart } from "./defillama";
 export const useBaseApy = (token: string) => {
   const { data, isLoading } = useChart(token);
   const baseApy = data?.[data?.length - 1].apy;
+  console.log("oooo", data);
 
   return {
     baseApy,
