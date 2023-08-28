237197af8d9e4cf15245bd7e4feb9c740a67a605
dev-aly3n <dev.aly3n@gmail.com>
2023-08-28 19:29:12 +0330

    fix: use 3 items before last for base apy

--- src/hooks/useBaseApy.ts
+++ src/hooks/useBaseApy.ts
@@ -2,7 +2,7 @@ import { useChart } from "./defillama";
 
 export const useBaseApy = (token: string) => {
   const { data, isLoading } = useChart(token);
-  const baseApy = data?.[data?.length - 1].apy;
+  const baseApy = data?.[data?.length - 3].apy;
 
   return {
     baseApy,
