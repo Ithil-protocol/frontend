6a5f4135b8954d52cb7b7a612c639b6ffec7fa2a
dev-aly3n <dev.aly3n@gmail.com>
2023-09-14 18:47:41 +0330

    feat: add itokens

---
+++ src/assets/imgs.ts
@@ -0,0 +1,8 @@
+import ITHIL from "./iTokens/ITHIL.png";
+import iDAI from "./iTokens/iDAI.png";
+import iWBTC from "./iTokens/iDAI.png";
+import iUSDC from "./iTokens/iUSDC.png";
+import iUSDT from "./iTokens/iUSDT.png";
+import iWETH from "./iTokens/iWETH.png";
+
+export const iTokenIcons = { iDAI, iUSDC, iUSDT, iWBTC, iWETH, ITHIL };
