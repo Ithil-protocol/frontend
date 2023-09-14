00c54bc51fbb94fbc4ce16710332bb3ab6b7fc49
dev-aly3n <dev.aly3n@gmail.com>
2023-09-14 18:26:06 +0330

    feat: addToken  function

--- src/utils/addIthil.ts
+++ src/utils/addIthil.ts
@@ -1,6 +1,7 @@
 import { getWalletClient } from "wagmi/actions";
 
 import contracts from "@/deploy/contracts.json";
+import { Token } from "@/types/onchain.types";
 
 export const addIthilToWallet = async () => {
   const walletClient = await getWalletClient();
@@ -11,6 +12,22 @@ export const addIthilToWallet = async () => {
         address: contracts.ithil,
         decimals: 18,
         symbol: "ITHIL",
+        image:
+          "https://raw.githubusercontent.com/Ithil-protocol/landing/master/src/assets/images/favicon-96x96.png",
+      },
+    });
+  }
+};
+
+export const addTokenToWallet = async (token: Token) => {
+  const walletClient = await getWalletClient();
+  if (walletClient) {
+    await walletClient.watchAsset({
+      type: "ERC20",
+      options: {
+        address: token.tokenAddress,
+        decimals: token.decimals,
+        symbol: token.name,
         image:
           "https://raw.githubusercontent.com/Ithil-protocol/landing/master/src/assets/images/favicon-96x96.png",
       },
