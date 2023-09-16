b5cb573cbb7b847323c1bc73e77156e44b492df0
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2023-09-16 19:28:58 +0330

    fix : number of dummy loader in active state

--- src/containers/Dashboard/Table/index.tsx
+++ src/containers/Dashboard/Table/index.tsx
@@ -149,7 +149,7 @@ const Table: FC<Props> = ({ columns, activeView }) => {
           {isLoadingPositions &&
             activeView === "Active" &&
             Array.from({ length: 4 }).map((_, index) => (
-              <TRowLoading tdCount={5} key={index} />
+              <TRowLoading tdCount={6} key={index} />
             ))}
           {isLoadingClosed &&
             activeView === "Closed" &&
