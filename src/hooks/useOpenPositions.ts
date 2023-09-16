8c94b9af2ce7cc5287cf80f6f24701346d1cec61
Arian Hamdi <arian.hamdi@gmail.com>
2023-09-16 19:29:24 +0330

    fix: crash

--- src/hooks/useOpenPositions.ts
+++ src/hooks/useOpenPositions.ts
@@ -50,9 +50,9 @@ export const useAaveOpenPositions = () => {
     const amount = agreement?.loans[0].amount;
     const margin = agreement?.loans[0].margin;
     const quoteResult = quotes?.[i].result as unknown[] as bigint[];
-    const quote = quoteResult?.[0];
+    const quote = quoteResult?.[0] || 0n;
     const feeResult = fees?.[i].result as unknown[] as bigint[];
-    const fee = feeResult?.[0];
+    const fee = feeResult?.[0] || 0n;
 
     const isPnlLoading =
       isQuotesLoading || isFeesLoading || isAgreementsLoading;
