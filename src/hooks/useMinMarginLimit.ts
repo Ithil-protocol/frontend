589989454747b3b170cf691927f2668e33fc538c
dev-aly3n <dev.aly3n@gmail.com>
2023-09-18 14:26:21 +0330

    feat: add useMinMarginLimit to aave

---
+++ src/hooks/useMinMarginLimit.ts
@@ -0,0 +1,32 @@
+import { Address, parseUnits } from "viem";
+import { useContractRead } from "wagmi";
+
+import { aaveABI } from "@/abi";
+import { Asset } from "@/types";
+
+interface Props {
+  asset: Asset;
+  serviceAddress: Address;
+  abi: typeof aaveABI;
+  inputAmount: string;
+}
+
+export const useMinMarginLimit = ({
+  abi,
+  asset,
+  serviceAddress,
+  inputAmount,
+}: Props) => {
+  const { data: minMargin, isLoading: isMinMarginLoading } = useContractRead({
+    args: [asset.tokenAddress],
+    address: serviceAddress,
+    abi,
+    functionName: "minMargin",
+  });
+  const bigintAmount = parseUnits(inputAmount ?? "0", asset.decimals);
+  const isLessThanMinimumMarginError = minMargin
+    ? bigintAmount < minMargin
+    : false;
+
+  return { isLessThanMinimumMarginError, isMinMarginLoading };
+};
